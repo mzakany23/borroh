2d5ece9c92308a0062cab097007bbedddda9ec6f
Michael Zakany <mzakany@gmail.com>
2015-04-09 13:25:44 -0400

    m

--- borroh/home/views.py
+++ borroh/home/views.py
@@ -7,7 +7,7 @@ from django.template import *
 
 def home(request):
 	featured_products = Product.objects.filter(featured=True)
-	context = {'login_form' : LoginForm, 'register' : RegisterUserForm}
+	context = {'login_form' : LoginForm, 'register' : RegisterUserForm, 'featured_products' : featured_products}
 	template = 'home/index.html'
 	return render(request,template,context)
 
